53aa99485f557f2c4e3af48b39e8765605b28f10
Tommins-IT <tomm.pydev@gmail.com>
2021-12-26 17:16:11 +0100

    Updated utilities emoji

--- cogs/help.py
+++ cogs/help.py
@@ -13,7 +13,7 @@ class HelpDropdown(diskord.ui.Select):
             diskord.SelectOption(label="Moderation", description="See al the moderation commands.",
                                  emoji="<:moderation:918837967957663765>"),
             diskord.SelectOption(label="Utilities", description="See all the utilities commands.",
-                                 emoji="<:utilities:918837614092636181>")
+                                 emoji="<:utilities:924694038177792010>")
         ]
 
         super().__init__(placeholder="Select a category...", min_values=1, max_values=1, options=options)
@@ -21,37 +21,9 @@ class HelpDropdown(diskord.ui.Select):
     async def callback(self, interaction: diskord.Interaction):
         msg = interaction.message
 
-        if self.values[0] == "Utilities":
-            embed = diskord.Embed(description=f"""
-
-`{prefix}calc <num_1> <operation> <num_2>` ➜ perform a mathematical operation.
-`{prefix}raw [message_id]` ➜ get raw text of a message (you can reply to a message).
-`{prefix}poll <question> <options>` ➜ create a poll with a maximum of 10 options.
-Example: `{prefix}poll "Do you like the bot?" Yes, a lot! // Yeah // No`
-`{prefix}addrole <user> <role>` ➜ add a role to a user (aliases: `ar`).
-`{prefix}removerole <user> <role>` ➜ remove a role from a user (aliases: `rr`).
-
-            """, color=embed_color)
-
-            await msg.edit(content="<:utilities:918837614092636181> **Utilities commands**", embed=embed)
-        if self.values[0] == "Moderation":
-            embed = diskord.Embed(description=f"""
-
-`{prefix}lock <#channel>` ➜ lock a text channel.
-`{prefix}unlock <#channel>` ➜ unlock a text channel.
-`{prefix}kick <user> [reason]` ➜ kick a user.
-`{prefix}ban <user> [reason]` ➜ ban a user.
-`{prefix}mute <user> [reason]` ➜ mute a user.
-`{prefix}tempmute <user> <duration> [reason]` ➜ tempmute a user (aliases: `tmute`).
-`{prefix}unmute <user>` ➜ unmute a user.
-`{prefix}clear <amount>` ➜ clear the specofied amount of messages.
-`{prefix}slowmode <seconds>` ➜ set the slowmode to the specified seconds.
-
-            """, color=embed_color)
-
-            await msg.edit(content="<:moderation:918837967957663765> **Moderation commands**", embed=embed)
         if self.values[0] == "Miscellaneous":
-            embed = diskord.Embed(description=f"""
+            embed = diskord.Embed(
+                description=f"""
 
 `{prefix}ping` ➜ get the bot's latency.
 `{prefix}userinfo [user]` ➜ get information about a user (aliases: `ui`).
@@ -66,9 +38,48 @@ Example: `{prefix}poll "Do you like the bot?" Yes, a lot! // Yeah // No`
 `{prefix}support` ➜ get the support server's link.
 `{prefix}vote` ➜ vote RedEagle.
 
-            """, color=embed_color)
+            """,
+                color=embed_color
+            )
 
             await msg.edit(content="<:miscellaneous:918838334292389908> **Miscellaneous commands**", embed=embed)
+
+        elif self.values[0] == "Moderation":
+            embed = diskord.Embed(
+                description=f"""
+
+`{prefix}lock <#channel>` ➜ lock a text channel.
+`{prefix}unlock <#channel>` ➜ unlock a text channel.
+`{prefix}kick <user> [reason]` ➜ kick a user.
+`{prefix}ban <user> [reason]` ➜ ban a user.
+`{prefix}mute <user> [reason]` ➜ mute a user.
+`{prefix}tempmute <user> <duration> [reason]` ➜ tempmute a user (aliases: `tmute`).
+`{prefix}unmute <user>` ➜ unmute a user.
+`{prefix}clear <amount>` ➜ clear the specofied amount of messages.
+`{prefix}slowmode <seconds>` ➜ set the slowmode to the specified seconds.
+
+            """,
+                color=embed_color
+            )
+
+            await msg.edit(content="<:moderation:918837967957663765> **Moderation commands**", embed=embed)
+
+        elif self.values[0] == "Utilities":
+            embed = diskord.Embed(
+                description=f"""
+
+`{prefix}calc <num_1> <operation> <num_2>` ➜ perform a mathematical operation.
+`{prefix}raw [message_id]` ➜ get raw text of a message (you can reply to a message).
+`{prefix}poll <question> <options>` ➜ create a poll with a maximum of 10 options.
+Example: `{prefix}poll "Do you like the bot?" Yes, a lot! // Yeah // No`
+`{prefix}addrole <user> <role>` ➜ add a role to a user (aliases: `ar`).
+`{prefix}removerole <user> <role>` ➜ remove a role from a user (aliases: `rr`).
+
+            """,
+                color=embed_color
+            )
+
+            await msg.edit(content="<:utilities:924694038177792010> **Utilities commands**", embed=embed)
 
 class HelpDropdownView(diskord.ui.View):
     def __init__(self):
