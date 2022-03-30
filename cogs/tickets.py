42a52d4c5e67b0b9930b29b65d8f7fc207cc10fa
Tommodev <tomm.pydev@gmail.com>
2022-03-30 20:03:12 +0200

    Code cleanup

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from discord.commands import SlashCommandGroup, Option
-import sqlite3
 import datetime
 import asyncio
 
@@ -17,34 +16,35 @@ class OpenTicket(discord.ui.View):
         button.disabled = True
         await interaction.response.edit_message(view=self)
 
-        db = sqlite3.connect("main.db")
-        cursor = db.cursor()
+        cursor = self.client.db.cursor()
         cursor.execute(f"SELECT role_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
         result = cursor.fetchone()
 
-        ticketrole = interaction.guild.get_role(result[0])
+        ticket_role = interaction.guild.get_role(result[0])
         user = self.client.get_user(int(interaction.channel.topic[9:]))
 
         await interaction.channel.set_permissions(interaction.guild.default_role, view_channel=False)
         await interaction.channel.set_permissions(user, send_messages=True, view_channel=True)
-        await interaction.channel.set_permissions(ticketrole, send_messages=True, view_channel=True)
+        await interaction.channel.set_permissions(ticket_role, send_messages=True, view_channel=True)
 
-        await interaction.channel.send(f"This ticket has been reopened by {interaction.user.mention}.\n"
-                                       "To delete the ticket, press the `Delete` button of the attached message.")
+        await interaction.channel.send(
+            f"This ticket has been reopened by {interaction.user.mention}.\n"
+            "To delete the ticket, press the `Delete` button of the attached message."
+        )
 
         cursor.execute(f"SELECT logs_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
-        result1 = cursor.fetchone()
+        channel_id = cursor.fetchone()
 
-        logschannel = self.client.get_channel(result1[0])
+        logs_channel = self.client.get_channel(channel_id[0])
 
-        embedlogs = discord.Embed(
+        log_embed = discord.Embed(
             title="Ticket reopened",
             description=f"{interaction.user.mention} reopened the ticket {interaction.channel.mention}.",
             color=embed_color
         )
-        embedlogs.timestamp = datetime.datetime.utcnow()
+        log_embed.timestamp = datetime.datetime.utcnow()
 
-        await logschannel.send(embed=embedlogs)
+        await logs_channel.send(embed=log_embed)
 
 class NewTicketButtons(discord.ui.View):
     def __init__(self, client):
@@ -56,56 +56,55 @@ class NewTicketButtons(discord.ui.View):
         button.disabled = True
         await interaction.response.edit_message(view=self)
 
-        db = sqlite3.connect("main.db")
-        cursor = db.cursor()
+        cursor = self.client.db.cursor()
         cursor.execute(f"SELECT role_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
         result = cursor.fetchone()
 
-        ticketrole = interaction.guild.get_role(result[0])
+        ticket_role = interaction.guild.get_role(result[0])
         user = self.client.get_user(int(interaction.channel.topic[9:]))
 
         await interaction.channel.set_permissions(interaction.guild.default_role, view_channel=False)
         await interaction.channel.set_permissions(user, send_messages=False, view_channel=True)
-        await interaction.channel.set_permissions(ticketrole, send_messages=True, view_channel=True)
+        await interaction.channel.set_permissions(ticket_role, send_messages=True, view_channel=True)
 
-        await interaction.channel.send(f"This ticket has been closed by {interaction.user.mention}.\n"
-                                       "To reopen the ticket, click on the button below.",
-                                       view=OpenTicket(self.client))
+        await interaction.channel.send(
+            f"This ticket has been closed by {interaction.user.mention}.\n"
+            "To reopen the ticket, click on the button below.", view=OpenTicket(self.client)
+        )
 
         cursor.execute(f"SELECT logs_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
-        result1 = cursor.fetchone()
+        channel_id = cursor.fetchone()
 
-        logschannel = self.client.get_channel(result1[0])
+        logs_channel = self.client.get_channel(channel_id[0])
 
-        embedlogs = discord.Embed(
+        log_embed = discord.Embed(
             title="Ticket closed",
             description=f"{interaction.user.mention} closed the ticket {interaction.channel.mention}.",
             color=embed_color
         )
-        embedlogs.timestamp = datetime.datetime.utcnow()
+        log_embed.timestamp = datetime.datetime.utcnow()
 
-        await logschannel.send(embed=embedlogs)
+        await logs_channel.send(embed=log_embed)
 
     @discord.ui.button(label="Delete", emoji="❌", style=discord.ButtonStyle.grey, custom_id="delete_ticket")
     async def delete(self, button: discord.ui.Button, interaction: discord.Interaction):
-        db = sqlite3.connect("main.db")
-        cursor = db.cursor()
+        cursor = self.client.db.cursor()
         cursor.execute(f"SELECT logs_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
         result = cursor.fetchone()
 
-        logschannel = self.client.get_channel(result[0])
+        logs_channel = self.client.get_channel(result[0])
 
-        embedlogs = discord.Embed(
+        log_embed = discord.Embed(
             title="Ticket deleted",
             description=f"{interaction.user.mention} closed the ticket #{str(interaction.channel.name)}.",
             color=embed_color
         )
-        embedlogs.timestamp = datetime.datetime.utcnow()
+        log_embed.timestamp = datetime.datetime.utcnow()
 
         await interaction.response.send_message("This ticket will be deleted in 5 seconds.")
         await asyncio.sleep(5)
         await interaction.channel.delete()
-        await logschannel.send(embed=embedlogs)
+        await logs_channel.send(embed=log_embed)
 
 class NewTicket(discord.ui.View):
     def __init__(self, client):
@@ -113,9 +112,8 @@ class NewTicket(discord.ui.View):
         super().__init__(timeout=None)
 
     @discord.ui.button(label="Open a ticket", emoji="📩", style=discord.ButtonStyle.green, custom_id="new_ticket")
-    async def newticket(self, button: discord.ui.Button, interaction: discord.Interaction):
-        db = sqlite3.connect("main.db")
-        cursor = db.cursor()
+    async def new_ticket(self, button: discord.ui.Button, interaction: discord.Interaction):
+        cursor = self.client.db.cursor()
         cursor.execute(f"SELECT role_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
         result = cursor.fetchone()
 
@@ -125,21 +123,21 @@ class NewTicket(discord.ui.View):
                 "admin to solve this issue.", ephemeral=True
             )
         else:
-            ticketrole = interaction.guild.get_role(result[0])
+            ticket_role = interaction.guild.get_role(result[0])
 
             cursor.execute(f"SELECT category_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
-            result1 = cursor.fetchone()
+            result = cursor.fetchone()
 
-            if result1 is None:
+            if result is None:
                 category = None
             else:
-                category = self.client.get_channel(result1[0])
+                category = self.client.get_channel(result[0])
 
             channel = await interaction.guild.create_text_channel(f"{interaction.user.name}-ticket", category=category)
 
             await channel.set_permissions(interaction.user, send_messages=True, view_channel=True)
             await channel.set_permissions(interaction.guild.default_role, view_channel=False)
-            await channel.set_permissions(ticketrole, send_messages = True, view_channel = True)
+            await channel.set_permissions(ticket_role, send_messages = True, view_channel = True)
             
             await channel.edit(topic=f"User ID: {interaction.user.id}")
 
@@ -152,26 +150,26 @@ class NewTicket(discord.ui.View):
 
             await interaction.response.send_message(f"Ticket opened! {channel.mention}", ephemeral=True)
 
-            message = await channel.send(content=f"{interaction.user.mention} {ticketrole.mention}", embed=embed,
+            message = await channel.send(content=f"{interaction.user.mention} {ticket_role.mention}", embed=embed,
                                          view=NewTicketButtons(self.client))
             await message.pin()
 
             cursor.execute(f"SELECT logs_id FROM Tickets WHERE guild_id = {interaction.guild.id}")
-            result2 = cursor.fetchone()
+            channel_id = cursor.fetchone()
 
-            if result2 is None:
+            if channel_id is None:
                 return
             else:
-                logschannel = self.client.get_channel(result2[0])
+                logs_channel = self.client.get_channel(channel_id[0])
 
-                embedlogs = discord.Embed(
+                log_embed = discord.Embed(
                     title="Ticket opened",
                     description=f"{interaction.user.mention} opened the ticket {channel.mention}.",
                     color=embed_color
                 )
-                embedlogs.timestamp = datetime.datetime.utcnow()
+                log_embed.timestamp = datetime.datetime.utcnow()
 
-                await logschannel.send(embed=embedlogs)
+                await logs_channel.send(embed=log_embed)
 
 class Tickets(commands.Cog):
     def __init__(self, client):
@@ -204,23 +202,22 @@ class Tickets(commands.Cog):
             ctx,
             role: Option(discord.Role, "Role that will have access to tickets.")
     ):
-        db = sqlite3.connect("main.db")
-        cursor = db.cursor()
+        cursor = self.client.db.cursor()
         cursor.execute(f"SELECT role_id FROM Tickets WHERE guild_id = {ctx.guild.id}")
         result = cursor.fetchone()
 
         if result is None:
-            sql = "INSERT INTO Tickets(guild_id, role_id) VALUES(?,?)"
-            val = (ctx.guild.id, role.id)
-            await ctx.respond(f"Role has been set to `{role.name}`.")
+            cursor.execute(
+                f"INSERT INTO Tickets(guild_id, role_id) VALUES(?,?)", (ctx.guild.id, role.id)
+            )
         else:
-            sql = "UPDATE Tickets SET role_id = ? WHERE guild_id = ?"
-            val = (role.id, ctx.guild.id)
-            await ctx.respond(f"Role has been set to `{role.name}`.")
+            cursor.execute(
+                "UPDATE Tickets SET role_id = ? WHERE guild_id = ?", (role.id, ctx.guild.id)
+            )
 
-        cursor.execute(sql, val)
-        db.commit()
-        cursor.close()
+        self.client.db.commit()
+
+        await ctx.respond(f"Role has been set to `{role.name}`.")
 
     @tickets_group.command(description="Set the logs channel for ticket events.")
     @commands.has_permissions(administrator=True)
@@ -229,23 +226,22 @@ class Tickets(commands.Cog):
             ctx,
             channel: Option(discord.TextChannel, "Channel where events will be logged.")
     ):
-        db = sqlite3.connect("main.db")
-        cursor = db.cursor()
+        cursor = self.client.db.cursor()
         cursor.execute(f"SELECT logs_id FROM Tickets WHERE guild_id = {ctx.guild.id}")
         result = cursor.fetchone()
 
         if result is None:
-            sql = "INSERT INTO Tickets(guild_id, logs_id) VALUES(?,?)"
-            val = (ctx.guild.id, channel.id)
-            await ctx.respond(f"Channel has been set to {channel.mention}.")
+            cursor.execute(
+                f"INSERT INTO Tickets(guild_id, logs_id) VALUES(?,?)", (ctx.guild.id, channel.id)
+            )
         else:
-            sql = "UPDATE Tickets SET logs_id = ? WHERE guild_id = ?"
-            val = (channel.id, ctx.guild.id)
-            await ctx.respond(f"Channel has been set to {channel.mention}.")
+            cursor.execute(
+                "UPDATE Tickets SET logs_id = ? WHERE guild_id = ?", (channel.id, ctx.guild.id)
+            )
 
-        cursor.execute(sql, val)
-        db.commit()
-        cursor.close()
+        self.client.db.commit()
+
+        await ctx.respond(f"Logs channel has been set to {channel.mention}.")
 
     @tickets_group.command(description="Set the category where tickets will be created.")
     @commands.has_permissions(administrator=True)
@@ -254,23 +250,22 @@ class Tickets(commands.Cog):
             ctx,
             category: Option(discord.CategoryChannel, "Category where tickets will be created.")
     ):
-        db = sqlite3.connect("main.db")
-        cursor = db.cursor()
+        cursor = self.client.db.cursor()
         cursor.execute(f"SELECT category_id FROM Tickets WHERE guild_id = {ctx.guild.id}")
         result = cursor.fetchone()
 
         if result is None:
-            sql = "INSERT INTO Tickets(guild_id, category_id) VALUES(?,?)"
-            val = (ctx.guild.id, category.id)
-            await ctx.respond(f"Category has been set to `{category.name}`.")
+            cursor.execute(
+                f"INSERT INTO Tickets(guild_id, category_id) VALUES(?,?)", (ctx.guild.id, category.id)
+            )
         else:
-            sql = f"UPDATE Tickets SET category_id = ? WHERE guild_id = ?"
-            val = (category.id, ctx.guild.id)
-            await ctx.respond(f"Category has been set to `{category.name}`.")
+            cursor.execute(
+                "UPDATE Tickets SET category_id = ? WHERE guild_id = ?", (category.id, ctx.guild.id)
+            )
 
-        cursor.execute(sql, val)
-        db.commit()
-        cursor.close()
+        self.client.db.commit()
+
+        await ctx.respond(f"Category has been set to `{category.name}`.")
 
     @tickets_group.command(description="Send in a channel the panel for opening tickets.")
     @commands.has_permissions(administrator=True)
